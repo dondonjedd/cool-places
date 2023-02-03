2c0cf4fb8c320db7b70d0ad9d5ed6e2ca1f4bba0
dondonjedd <70884225+dondonjedd@users.noreply.github.com>
2023-02-03 13:49:02 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:cool_places/providers/prov_places.dart';
+import 'package:cool_places/screens/places_list_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (ctx) => ProvPlaces(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            colorScheme: ThemeData.light()
+                .colorScheme
+                .copyWith(secondary: Colors.brown[300])),
+        home: const PlacesListScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Cool Places"), actions: <Widget>[
+        IconButton(onPressed: () {}, icon: const Icon(Icons.add))
+      ]),
+      body: const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
