084698a2348ed18ca5de76348f5a441ac7db37e6
dondonjedd <70884225+dondonjedd@users.noreply.github.com>
2023-02-04 19:25:02 +0800

    handle error when no picture is taken

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -20,12 +20,15 @@ class _ImageInputState extends State<ImageInput> {
     final picker = ImagePicker();
     final imageFile =
         await picker.pickImage(source: ImageSource.camera, maxWidth: 600);
+    if (imageFile == null) {
+      return;
+    }
     setState(() {
-      _storedImage = File(imageFile!.path);
+      _storedImage = File(imageFile.path);
     });
 
     final appDir = await getApplicationDocumentsDirectory();
-    final fileName = basename(imageFile!.path);
+    final fileName = basename(imageFile.path);
     final savedImage = await _storedImage!.copy('${appDir.path}/$fileName');
     widget.onSelectImage(savedImage);
   }
