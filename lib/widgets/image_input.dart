49cdd6d3917ce11d520483d4eb94d41c9f8098a1
dondonjedd <70884225+dondonjedd@users.noreply.github.com>
2023-02-04 12:56:05 +0800

    Open camera in add place screen

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -11,6 +12,12 @@ class ImageInput extends StatefulWidget {
 
 class _ImageInputState extends State<ImageInput> {
   File? _storedImage;
+
+  _takePicture() async {
+    final picker = ImagePicker();
+    final imageFile =
+        await picker.pickImage(source: ImageSource.camera, maxWidth: 600);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +45,7 @@ class _ImageInputState extends State<ImageInput> {
           width: 10,
         ),
         TextButton.icon(
-          onPressed: () {},
+          onPressed: _takePicture,
           icon: const Icon(Icons.camera),
           label: const Text('Take Picture'),
           style: TextButton.styleFrom(
